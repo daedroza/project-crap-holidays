31788735d34d130e27548106b8186927a64282e3
Kalpaj Chaudhari <Daedroza@gmail.com>
2016-09-14 16:45:42 +0530

    Teach sorting subject toppers
    Signed-off-by: Kalpaj Chaudhari <Daedroza@gmail.com>

--- Test.java
+++ Test.java
@@ -15,7 +15,7 @@ class Student extends Marks {
 public class Test {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int x,y;
+		int x,y; //Used somewhere in for-loops
 		System.out.println("Number of students : ");
 		int n;
 		n = sc.nextInt();
@@ -50,7 +50,7 @@ public class Test {
 			System.out.println("Total marks for student"+s[i].name+" is "+s[i].total_marks);
 		}
 		//Sort the objects according to total_marks
-		for(x = 0;x < n; x++) {
+		for(x = 0;x <= n - 1; x++) {
 			for(y = 0;y < n - 1 ; y++) {
 				if(s[y].total_marks < s[y+1].total_marks) {
 					t=s[y];
@@ -63,6 +63,104 @@ public class Test {
 		for(int i = 0; i < n; i++) {
 			System.out.println("Name : "+s[i].name+" Total Marks : "+s[i].total_marks);
 		}
+		//Subject Topper after Sorting
+		int topper_dlda = s[0].dlda;
+		int pos_dlda;
+		int topper_ds = s[0].ds;
+		int pos_ds;
+		int topper_math = s[0].math;
+		int pos_math;
+		int topper_oopm = s[0].oopm;
+		int pos_oopm;
+		int topper_eccf = s[0].eccf;
+		int pos_eccf;
+		int topper_dst = s[0].dst;
+		int pos_dst;
+		int topper_total = s[0].total_marks;
+		int pos_total;
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_dlda < s[i].dlda)
+			{
+				topper_dlda = s[i].dlda;
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_dlda == s[i].dlda)
+			{
+				pos_dlda = i; 
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_math < s[i].math)
+			{
+				topper_math = s[i].math;
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_math == s[i].math)
+			{
+				pos_math = i; 
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_oopm < s[i].oopm)
+			{
+				topper_oopm = s[i].oopm;
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_oopm == s[i].oopm)
+			{
+				pos_oopm = i; 
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_ds < s[i].ds)
+			{
+				topper_ds = s[i].ds;
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_ds == s[i].ds)
+			{
+				pos_ds = i; 
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_dst < s[i].dst)
+			{
+				topper_dst = s[i].dst;
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_dst == s[i].dst)
+			{
+				pos_dst = i; 
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_eccf < s[i].eccf)
+			{
+				topper_eccf = s[i].eccf;
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_eccf == s[i].eccf)
+			{
+				pos_eccf = i; 
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_total < s[i].total_marks)
+			{
+				topper_total = s[i].total_marks;
+			}
+		}
+		for(int i = 0; i <= n - 1; i++) {
+			if(topper_total == s[i].total_marks)
+			{
+				pos_total = i; 
+			}
 		}
 	}
 }
