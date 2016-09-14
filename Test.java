aef4b36c067cdf6f9b36de8401565c954fe5caae
Kalpaj Chaudhari <Daedroza@gmail.com>
2016-09-14 14:16:31 +0530

    v3: Add total marks *Also fix string
    Signed-off-by: Kalpaj Chaudhari <Daedroza@gmail.com>

--- Test.java
+++ Test.java
@@ -11,17 +11,19 @@ public class Test
 		System.out.println("How many students you want to add?");
 		n = sc.nextInt();
 		String[] name = new String[n];
-        int[] dlda = new int[n];
-        int[] ds = new int[n];
-        int[] math = new int[n];
-        int[] oopm = new int[n];
-        int[] eccf = new int[n];
-        int[] dst = new int[n];
+		String[] sorted_name = new String[n];
+		int[] dlda = new int[n];
+		int[] ds = new int[n];
+		int[] math = new int[n];
+		int[] oopm = new int[n];
+		int[] eccf = new int[n];
+		int[] dst = new int[n];
+		int[] total_marks = new int[n];
 		//Get name and marks
 		for(i = 0; i <= n - 1; i++)
 		{
 			System.out.println("Enter the name of student : ");
-			name[i] = sc.nextLine();
+			name[i] = sc.next();
 			System.out.println("You are entering the scores of "+name[i]);
 			System.out.println("Marks for DLDA : ");
 			dlda[i] = sc.nextInt();
@@ -36,6 +38,12 @@ public class Test
 			System.out.println("Marks for DST : ");
 			dst[i] = sc.nextInt();
 			System.out.println("You have successfully entered the scores of the student "+name[i]);
+		}
+		//Calculate total marks
+		for(i = 0; i <= n - 1; i++)
+		{
+			total_marks[i] = dlda[i] + ds[i] + math[i] + oopm[i] + eccf[i] + dst[i];
+			System.out.println("Total marks is for the student : "+name[i]+" is "+total_marks[i]);
 		}
 	}
 }
