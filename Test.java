36c8a62cb463721b607231ca2d5e8080d61d1bd3
Kalpaj Chaudhari <Daedroza@gmail.com>
2016-09-14 17:19:28 +0530

    Print sub toppers, overall toppers and Count number of passed/failed students
    Signed-off-by: Kalpaj Chaudhari <Daedroza@gmail.com>

--- Test.java
+++ Test.java
@@ -6,6 +6,7 @@ class Name {
 
 class Marks extends Name {
 	public int dlda, ds, math, oopm, eccf, dst;
+	public boolean result;
 }
 
 class Student extends Marks {
@@ -16,6 +17,7 @@ public class Test {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int x,y; //Used somewhere in for-loops
+		int countp = 0, countf = 0;
 		System.out.println("Number of students : ");
 		int n;
 		n = sc.nextInt();
@@ -44,11 +46,12 @@ public class Test {
 			System.out.println("You have successfully entered the scores of the student "+s[i].name);
 			System.out.println("\n");
 		}
-		System.out.println("\n");
+		//Calculate Total Marks
 		for(int i = 0; i <= n - 1; i++) {
 			s[i].total_marks = s[i].dlda + s[i].ds + s[i].math + s[i].oopm + s[i].eccf + s[i].dst;
-			System.out.println("Total marks for student"+s[i].name+" is "+s[i].total_marks);
+			System.out.println("Total marks for "+s[i].name+" is "+s[i].total_marks);
 		}
+		System.out.println("\n");
 		//Sort the objects according to total_marks
 		for(x = 0;x <= n - 1; x++) {
 			for(y = 0;y < n - 1 ; y++) {
@@ -61,23 +64,24 @@ public class Test {
 		}
 		System.out.println("The list sorted according to Total Marks : ");
 		for(int i = 0; i < n; i++) {
-			System.out.println("Name : "+s[i].name+" Total Marks : "+s[i].total_marks);
+			System.out.println("Name : "+s[i].name+", Total Marks : "+s[i].total_marks);
 		}
-		//Subject Topper after Sorting
+		System.out.println("\n");
+		//Subject Topper (Sorted List according to total_marks)
 		int topper_dlda = s[0].dlda;
-		int pos_dlda;
+		int pos_dlda = 0;
 		int topper_ds = s[0].ds;
-		int pos_ds;
+		int pos_ds = 0;
 		int topper_math = s[0].math;
-		int pos_math;
+		int pos_math = 0;
 		int topper_oopm = s[0].oopm;
-		int pos_oopm;
+		int pos_oopm = 0;
 		int topper_eccf = s[0].eccf;
-		int pos_eccf;
+		int pos_eccf = 0;
 		int topper_dst = s[0].dst;
-		int pos_dst;
+		int pos_dst = 0;
 		int topper_total = s[0].total_marks;
-		int pos_total;
+		int pos_total = 0;
 		for(int i = 0; i <= n - 1; i++) {
 			if(topper_dlda < s[i].dlda)
 			{
@@ -162,5 +166,23 @@ public class Test {
 				pos_total = i; 
 			}
 		}
+		//Print Subject Toppers(NERDS) && SCHOLAR PPL
+		System.out.println("DLDA Topper : "+s[pos_dlda].name+" and his/her score : "+s[pos_dlda].dlda);
+		System.out.println("DS Topper : "+s[pos_ds].name+" and his/her score : "+s[pos_ds].ds);
+		System.out.println("Math Topper : "+s[pos_math].name+" and his/her score : "+s[pos_math].math);
+		System.out.println("OOPM Topper : "+s[pos_oopm].name+" and his/her score : "+s[pos_oopm].oopm);
+		System.out.println("ECCF Topper : "+s[pos_eccf].name+" and his/her score : "+s[pos_eccf].eccf);
+		System.out.println("DST Topper : "+s[pos_dst].name+" and his/her score : "+s[pos_dst].dst);
+		System.out.println("Overall Topper : "+s[pos_total].name+" and his/her score : "+s[pos_total].total_marks);
+		System.out.println("\n");
+		//Passed or failed <- DESTROYS MANY PPLS LIFE
+		for(int i = 0; i <= n - 1; i++) {
+			if(s[i].total_marks > 210) {
+				countp++;
+			}else countf++;
+		}
+		System.out.println("No. of passed students = "+countp);
+		System.out.println("No. of failed students = "+countf);
+
 	}
 }
