e26b7bc35b5fc69e63d392700d8bdf889181f315
Kalpaj Chaudhari <daedroza@gmail.com>
2016-10-16 11:58:10 +0530

    merge new changes

--- Test.java
+++ Test.java
@@ -8,22 +8,25 @@ class MarksOutOfRangeException extends Exception {
 }
 /* Base Class */
 class Name {
-	static public String name, first_name, last_name, space = " ";
+	public String name = null, first_name = null, last_name = null, space = " ";
 }
 /* Marks Class */
 class Marks extends Name {
-	static public int dlda, ds, math, oopm, eccf, dst;
+	public int dlda, ds, math, oopm, eccf, dst;
 }
 /* Student Class which inherits our base class */
 class Student extends Marks {
-	static public int total_marks;
+	public int total_marks;
 }
-public class Test {
-
-	static int countp = 0, countf =0;
+class Stuff {
+	Student s[] = new Student[200];
+	Student t = new Student(); /* Temporary variable */
+	static int n = 0;
+	int countp = 0, countf = 0;
+	Scanner sc = new Scanner (System.in);
 	
-	public static int super_nextInt(Scanner sc) {
-		int temp = 0;
+	public int super_nextInt() {
+		int temp;
 		while(true) {
 			try { 
 				temp = sc.nextInt();
@@ -37,8 +40,8 @@ public class Test {
 				System.out.println("***Exception Occurred : NotMarksException***"); 
 				System.out.println("Enter the marks again : ");
 				/* nextInt() class doesn't assign ASCII value of alphabet
-				 * to integer variable. This character/string lies at the
-				 * end of memory buffer. nextInt() will keep on trying to
+				 * to integer variable. This character/string lies in 
+				 * memory buffer. nextInt() will keep on trying to
 				 * assign to integer variable causing an infinite loop.
 				 * We use a String garbage which will clear memory buffer
 				 * for us.
@@ -49,8 +52,9 @@ public class Test {
 		return temp;
 	}
 	
-	public static void take_input(Student s[], Scanner sc,int i) {
-
+	public void take_input() {
+			int i = n;
+			s[i] = new Student();
 			System.out.println("Enter the First Name of student : ");
 			s[i].first_name = sc.next();
 			System.out.println("Enter the Last Name of Student : ");
@@ -58,28 +62,30 @@ public class Test {
 			s[i].name = s[i].first_name + s[i].space + s[i].last_name;
 			System.out.println("You are entering the scores of "+s[i].name);
 			System.out.println("Marks for DLDA : ");
-			s[i].dlda = super_nextInt(sc);
+			s[i].dlda = super_nextInt();
 			System.out.println("Marks for DS : ");
-			s[i].ds = super_nextInt(sc);
+			s[i].ds = super_nextInt();
 			System.out.println("Marks for Math : ");
-			s[i].math = super_nextInt(sc);
+			s[i].math = super_nextInt();
 			System.out.println("Marks for OOPM : ");
-			s[i].oopm = super_nextInt(sc);
+			s[i].oopm = super_nextInt();
 			System.out.println("Marks for ECCF : ");
-			s[i].eccf = super_nextInt(sc);
+			s[i].eccf = super_nextInt();
 			System.out.println("Marks for DST : ");
-			s[i].dst = super_nextInt(sc);
+			s[i].dst = super_nextInt();
 			System.out.println("You have successfully entered the scores of the student "+s[i].name);
 			System.out.println("\n");
 			/* Calculate total marks */
-			for(int l = 0; i < n - 1; i++) {
+			for(int l = 0; l < n - 1; l++) {
 			s[i].total_marks = s[i].dlda + s[i].ds + s[i].math + s[i].oopm + s[i].eccf + s[i].dst;
 			System.out.println("Total marks for "+s[i].name+" is "+s[i].total_marks);
 		}
 		i++;
 	}
 	
-	public static void sort_totalmarks(Student s[], Student t) {
+	public void sort_totalmarks() {
+		System.out.println(s[0].name);
+		System.out.println(s[1].name);
 		for(int x = 0;x < n - 1; x++) {
 			for(int y = 0;y < n - 1 ; y++) {
 				if(s[y].total_marks < s[y+1].total_marks) {
@@ -91,20 +97,15 @@ public class Test {
 		}
 	}
 	
-	/* Was kept inside sub_topper initially.
-	 * Removed it here, so that Printer functions can access too
-	 * instead of passing them which is a tedious work.
-	 */
+	int pos_dlda = 0;
+	int pos_ds = 0;	
+	int pos_math = 0;
+	int pos_oopm = 0;
+	int pos_eccf = 0;
+	int pos_dst = 0;
+	int pos_total = 0;
 	
-	static int pos_dlda = 0;
-	static int pos_ds = 0;	
-	static int pos_math = 0;
-	static int pos_oopm = 0;
-	static int pos_eccf = 0;
-	static int pos_dst = 0;
-	static int pos_total = 0;
-	
-	public static int topper_dlda(Student s[]) {
+	public int topper_dlda() {
 		int topper_dlda = s[0].dlda;
 		for(int i = 0; i <= n - 1; i++) {
 			if(topper_dlda < s[i].dlda)
@@ -121,7 +122,7 @@ public class Test {
 		return pos_dlda;
 	}
 	
-	public static int topper_math(Student s[]) {
+	public int topper_math() {
 		int topper_math = s[0].math;
 		for(int i = 0; i <= n - 1; i++) {
 			if(topper_math < s[i].math)
@@ -138,7 +139,7 @@ public class Test {
 		return pos_math;
 	}
 	
-	public static int topper_oopm(Student s[]) {
+	public int topper_oopm() {
 		int topper_oopm = s[0].oopm;
 		for(int i = 0; i <= n - 1; i++) {
 			if(topper_oopm < s[i].oopm)
@@ -155,7 +156,7 @@ public class Test {
 		return pos_oopm;
 	}
 	
-	public static int topper_ds(Student s[]) {
+	public int topper_ds() {
 		int topper_ds = s[0].ds;
 		for(int i = 0; i <= n - 1; i++) {
 			if(topper_ds < s[i].ds)
@@ -172,7 +173,7 @@ public class Test {
 		return pos_ds;
 	}
 	
-	public static int topper_dst(Student s[]) {
+	public int topper_dst() {
 		int topper_dst = s[0].dst;
 		for(int i = 0; i <= n - 1; i++) {
 			if(topper_dst < s[i].dst)
@@ -189,7 +190,7 @@ public class Test {
 		return pos_dst;
 	}
 	
-	public static int topper_eccf(Student s[]) {
+	public int topper_eccf() {
 		int topper_eccf = s[0].eccf;
 		for(int i = 0; i <= n - 1; i++) {
 			if(topper_eccf < s[i].eccf)
@@ -206,7 +207,7 @@ public class Test {
 		return pos_eccf;
 	}
 	
-	public static int topper_total(Student s[]) {
+	public int topper_total() {
 		int topper_total = s[0].total_marks;
 		for(int i = 0; i <= n - 1; i++) {
 			if(topper_total < s[i].total_marks)
@@ -223,17 +224,17 @@ public class Test {
 		return pos_total;
 	}
 	
-	public static void topper(Student s[]) {
-		pos_dlda = topper_dlda(s);
-		pos_ds = topper_ds(s);
-		pos_dst = topper_dst(s);
-		pos_math = topper_math(s);
-		pos_oopm = topper_oopm(s);
-		pos_eccf = topper_eccf(s);
+	public void topper() {
+		pos_dlda = topper_dlda();
+		pos_ds = topper_ds();
+		pos_dst = topper_dst();
+		pos_math = topper_math();
+		pos_oopm = topper_oopm();
+		pos_eccf = topper_eccf();
 	}
 	
 	
-	public static void pass_fail(Student s[]) {
+	public void pass_fail() {
 		
 		for(int i = 0; i < n - 1; i++) {
 			if(s[i].total_marks >= 210) {
@@ -247,7 +248,7 @@ public class Test {
 		System.out.println("No. of failed students = "+countf);
 	}
 	
-	public static void printtofile(Student s[]) {
+	public void printtofile() {
 		try(PrintWriter out = new PrintWriter("txt.txt");) {
 			for(int i = 0; i < n - 1; i++) {
 				out.println();
@@ -288,13 +289,9 @@ public class Test {
 		}
 	}
 	
-	public static void main(String args[]) {
-		Scanner sc = new Scanner (System.in);
-		int n = 0;
+	public void menu() {
 		int choice;
-		int status = -999; /* Status flag */
-		Student s[] = new Student[200];
-		Student t = new Student(); /* Temporary variable */
+		
 		while(true) {
 			System.out.println("1 - Add Student");
 			System.out.println("2 - Sort according to total marks");
@@ -306,7 +303,7 @@ public class Test {
 			choice = sc.nextInt();
 			switch(choice) {
 				case 1 :
-				take_input(s, sc, n);
+				take_input();
 				n++;
 				break;
 				
@@ -314,25 +311,32 @@ public class Test {
 				if (n < 2) {
 					System.out.println("Add student first.");
 					System.out.println();
-				}else sort_totalmarks(s,t);
+				}else sort_totalmarks();
 
 				break;
 				
 				case 3 :
-				topper(s);
+				topper();
 				break;
 				
 				case 4 :
-				pass_fail(s);
+				pass_fail();
 				break;
 				
 				case 5 :
-				printtofile(s);
+				printtofile();
 				break;
 				
 				case 6 :
 				System.exit(0);
 			}
 		}
+	}
+}
+
+public class Test {
+	public static void main(String args[]) {
+		Stuff t1 = new Stuff();
+		t1.menu();
 	}
 }
